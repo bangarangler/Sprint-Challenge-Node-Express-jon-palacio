d6081ead6416aa3e227c974ac2568f0458a8759d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-22 13:11:46 -0400

    Added conditional rendering of route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { Route, Link } from "react-router-dom";
 import Projects from "./components/Projects/Projects.js";
 
 class App extends Component {
+  state = {
+    show: false
+  };
+
+  handleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
   render() {
-    return (
-      <div className="App">
-        <Projects />
-      </div>
-    );
+    let proj = null;
+    if (this.state.show) {
+      return (proj = (
+        <Route
+          pathname="/projects"
+          render={props => {
+            return <Projects />;
+          }}
+        />
+      ));
+    } else {
+      return (
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link onClick={this.handleShow} to="/projects">
+                  Projects
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <h1>Welcome to Front End</h1>
+          {proj}
+        </div>
+      );
+    }
   }
 }
 
